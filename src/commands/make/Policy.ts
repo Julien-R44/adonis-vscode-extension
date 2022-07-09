af17760f441e67dccb0c914167a23c5160587104
Julien <julien@ripouteau.com>
2022-07-09 11:31:18 +0200

    refactor: minor changes

--- src/commands/make/Policy.ts
+++ src/commands/make/Policy.ts
@@ -8,35 +8,33 @@ export class Policy extends BaseCommand {
     /**
      * Get the policy name
      */
-    let seederName = await this.getInput('Policy name')
-    if (!seederName) {
+    let policyName = await this.getInput('Policy name')
+    if (!policyName) {
       this.showError('Policy name is required.')
       return
     }
 
-    let command = `make:policy ${seederName}`
+    let command = `make:policy ${policyName}`
     const resourceModel = await this.getInput(
       'Enter the name of the resource model to authorize (Optional)'
     )
+
     if (resourceModel) {
       command += ` --resource-model=${resourceModel}`
 
-      // User model without resource model is not working as intended
-      // TODO: In this getInput i would put a Default Value of 'User' to match adonis cli
       let userModel = await this.getInput(
         'Enter the name of the user model to be authorized (Optional)'
       )
-      if (!userModel) {
-        userModel = 'User'
-      }
-      command += ` --user-model=${userModel}`
+
+      command += ` --user-model=${userModel || 'User'}`
     }
 
     const actions = await this.getListInput(
-      'Select the actions you want to authorize (Optional)',
+      'Select the actions you want to implement (Optional)',
       ['viewList', 'view', 'create', 'update', 'delete'],
       true
     )
+
     if (actions.length > 0) {
       command += ` --actions=${actions.join(',')}`
     }
