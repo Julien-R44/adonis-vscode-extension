b6f7e81db4ed8338b94374620c5b38d3d5ebd3e6
Julien <julien@ripouteau.com>
2022-08-20 23:56:54 +0200

    chore: fix tsup build by bundling all dependencies and externalize typescript

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,16 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  external: ['vscode'],
+  external: ['vscode', 'typescript'],
   format: ['cjs'],
   shims: false,
+  noExternal: [
+    '@poppinss/module-methods-extractor',
+    'docblock',
+    'fast-glob',
+    'lodash.groupby',
+    'vscode-ext-help-and-feedback-view',
+    'vscode-html-languageservice',
+    'vscode-languageserver-types',
+  ],
 })
