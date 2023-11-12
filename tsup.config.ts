baffa2b346e29026e17c4291a362241280101aad
Julien Ripouteau <julien@ripouteau.com>
2023-11-12 21:20:06 +0100

    chore: remove webpack

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,9 +6,16 @@ export default defineConfig({
   format: ['cjs'],
   shims: false,
   noExternal: [
+    'execa',
+    'dedent',
     'docblock',
     'fast-glob',
+    'slash',
+    'common-path-prefix',
+    '@julr/module-methods-extractor',
     'lodash.groupby',
-    'vscode-ext-help-and-feedback-view'
+    'vscode-ext-help-and-feedback-view',
+    "@babel/parser",
+    "@babel/traverse",
   ],
 })
