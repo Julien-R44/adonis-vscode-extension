e9136d6350a7f7c1316763b75ddd05b551c1b565
Thomas <31560900+0xtlt@users.noreply.github.com>
2025-07-20 21:44:21 +0200

    chore: update package-lock.json and regexes for controller linkers (#45)

--- src/utilities/regexes.ts
+++ src/utilities/regexes.ts
@@ -2,14 +2,14 @@
  * Used by inertia linker and suggester
  */
 export const inertiaRegex = new RegExp(/inertia\.render\(["'](.*?)["']/, 'g')
-export const inertiaCompletionRegex = new RegExp(/(?<=inertia\.render\()(["'])[^"']*\1/, 'g')
+export const inertiaCompletionRegex = new RegExp(/(?<=inertia\.render\()["'][^"']*["']/, 'g')
 
 /**
  * Used by controller suggester
  * Check if we are currently inside a controller link and capture the user input
  */
 export const controllerNameCompletionRegex = new RegExp(
-  /(?<=[Rr]outer?\.[A-Za-z]*\(["'][^"']*["']\s*,\s*)(["'])([^"'.]*)\1/,
+  /(?<=[Rr]outer?\s*\.[A-Za-z]*\(["'][^"']*["']\s*,\s*)(["'])([^"'.]*)\1/,
   'g'
 )
 
@@ -18,7 +18,7 @@ export const controllerNameCompletionRegex = new RegExp(
  * Check if we are currently inside a controller method link and capture the user input
  */
 export const controllerMethodCompletionRegex = new RegExp(
-  /(?<=[Rr]outer?\.[A-Za-z]*\(["'][^"']*["']\s*,\s*)(["'])([^"']*)\.([^"']*)\1/,
+  /(?<=[Rr]outer?\s*\.[A-Za-z]*\(["'][^"']*["']\s*,\s*)(["'])([^"']*)\.([^"']*)\1/,
   'g'
 )
 
@@ -27,6 +27,6 @@ export const controllerMethodCompletionRegex = new RegExp(
  * Find all controllers magic string that are being used inside a TS code
  */
 export const controllersRegex = new RegExp(
-  /(?<=[Rr]outer?\.[A-Za-z]*\(["'][^"']*["']\s*,\s*)(["'])([^"']*)\.?([^"']*)\1/,
+  /(?<=[Rr]outer?\s*\.[A-Za-z]*\(["'][^"']*["']\s*,\s*)(["'])([^"']*)\.?([^"']*)\1/,
   'g'
 )
